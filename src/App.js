61918b2dfe14b085ae89ca50db44e85b97a67ae5
Bruno Batalha <brunobatalha37@gmail.com>
2020-10-21 17:07:42 -0400

    Exclui arquivos não utilizados e cria estrutura de pastas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
+        <h1>BatalhaDev</h1>
+        <p>WebDesigner</p>
+        <p>Evoluindo sua empresa digitalmente</p>
+      </div>
+      <section>
+        <h2>Sobre mim</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá, sou Bruno Batalha, tenho o objetivo de aumentar a
+          visibilidade e lucros de pequenos e médios empreendedores no
+          mercado, com a criação de sites contendo a essência de seu
+          negócio.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src="" alt="BatalhaDev" />
+        <p>
+          Programador de sites, com mais de 3 anos de experiência em
+          programação. Formado como Técnico em Informática pelo
+          Instituto Federal de Educação Ciência e Tecnologia do
+          Amazonas
+        </p>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </section>
+      <section>
+        <h2>Portfolio</h2>
+        <div>
+          <div><img src="" alt="" srcSet="" /></div>
+          <a href="https://">Item 1</a>
+        </div>
+        <div>
+          <div><img src="" alt="" srcSet="" /></div>
+          <a href="https://">Item 2</a>
+        </div>
+        <div>
+          <div><img src="" alt="" srcSet="" /></div>
+          <a href="https//">Item 3</a>
+        </div>
+      </section>
+      <section>
+        <h2>Entre em contato</h2>
+        <a href="http://">
+          <img src="" alt="" srcSet="" />
         </a>
-      </header>
-    </div>
+        <a href="http://">
+          <img src="" alt="" srcSet="" />
+        </a>
+      </section>
+      <footer>
+        <p>BatalhaDev 2020 - Todos os direitos reservados</p>
+      </footer>
+    </>
   );
 }
 
